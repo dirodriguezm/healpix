56003494fc15d976e5edad2e0bc6347ab63407e5
Spencer Nelson <swnelson@uw.edu>
2021-01-13 16:04:45 -0800

    Use a cgo directive to always enable pkg-config

--- go_build.sh
+++ go_build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-export CGO_CXXFLAGS="$(pkg-config --cflags healpix_cxx libsharp)"
-export CGO_LDFLAGS="$(pkg-config --libs healpix_cxx libsharp)"
 go build -o ./build/tester -ldflags "-linkmode external -extldflags -static" ./cmd/tester
---
+++ internal/healpix_cxx/cgo_directive.go
@@ -0,0 +1,4 @@
+package healpix_cxx
+
+// #cgo pkg-config: healpix_cxx libsharp
+import "C"
